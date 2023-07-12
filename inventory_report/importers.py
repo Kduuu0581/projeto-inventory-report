f4e8f097d10f3d84e839098260274869ffdbcd30
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2023-07-12 15:44:40 -0300

    4. Crie a classe JsonImporter

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,6 +1,7 @@
 from typing import Dict, List, Type
 from abc import ABC, abstractmethod
 from inventory_report.product import Product
+import json
 
 
 class Importer(ABC):
@@ -12,8 +13,17 @@ class Importer(ABC):
         pass
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    def import_data(self) -> List[Product]:
+        data = []
+
+        with open(self.path) as file:
+            products = json.load(file)
+
+        for item in products:
+            data.append(Product(**item))
+
+        return data
 
 
 class CsvImporter:
