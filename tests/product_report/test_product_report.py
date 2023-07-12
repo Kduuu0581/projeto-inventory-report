b41f2583d89246deba266e9b90b52055ad7847b8
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2023-07-12 15:21:40 -0300

    2. Teste o relatório individual gerado por Produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,23 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    product = Product(
+        "1",
+        "Arroz",
+        "Marca",
+        "10/10/2020",
+        "10/10/2021",
+        "123456",
+        "Manter em local seco e arejado",
+    )
+    expected = (
+        f"The product {product.id} - {product.product_name} "
+        f"with serial number {product.serial_number} "
+        f"manufactured on {product.manufacturing_date} "
+        f"by the company {product.company_name} "
+        f"valid until {product.expiration_date} "
+        "must be stored according to the following instructions: "
+        f"{product.storage_instructions}."
+    )
+    assert str(product) == expected
