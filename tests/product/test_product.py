b8f45c025f3c6e4ea20f04e60de2277a8c512787
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2023-07-12 15:11:34 -0300

    1. Teste o construtor/inicializador do objeto Produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,20 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    product = Product(
+        "1",
+        "Arroz",
+        "Marca",
+        "10/10/2020",
+        "10/10/2021",
+        "123456",
+        "Manter em local seco e arejado",
+    )
+    assert product.id == "1"
+    assert product.product_name == "Arroz"
+    assert product.company_name == "Marca"
+    assert product.manufacturing_date == "10/10/2020"
+    assert product.expiration_date == "10/10/2021"
+    assert product.serial_number == "123456"
+    assert product.storage_instructions == "Manter em local seco e arejado"
