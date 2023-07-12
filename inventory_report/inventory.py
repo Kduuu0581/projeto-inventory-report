ab6466e6ac1721d745f23e0365f083246a3d91fd
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2023-07-12 15:54:21 -0300

    5. Crie a classe Inventory

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,16 @@
+from inventory_report.product import Product
+from typing import List, Optional
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[List[Product]] = None):
+        if data is None:
+            data = []
+        self._data = data.copy()
+
+    @property
+    def data(self) -> List[Product]:
+        return self._data.copy()
+
+    def add_product(self, product: List[Product]) -> None:
+        self._data.extend(product)
